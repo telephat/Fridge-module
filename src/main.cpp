7f817fd917b9456db27861b2a55b93749018e575
Andrey Kuznetsov <telephat@yandex.ru>
2025-05-03 01:28:51 +0300

    Added Elegant on air update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
-#include <DallasTemperature.h>
-#include <OneWire.h>
 #include <WiFi.h>               // ESP32 Core WiFi Library    
+#include <WiFiClient.h>
+#include <WebServer.h>
 #include <WiFiUdp.h>
 #include <SNMP_Agent.h>
 #include <SNMPTrap.h>
+#include <ElegantOTA.h>
+
+#include <DallasTemperature.h>
+#include <OneWire.h>
 
 #define ONE_WIRE_BUS 22
+#define RELAY_PIN 32
 
 OneWire oneWire(ONE_WIRE_BUS);
-
+WebServer server(80); //Webserver
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 DeviceAddress ds_term1, ds_term2;
@@ -20,16 +25,34 @@ const char* password = "19316632";
 WiFiUDP udp;
 SNMPAgent snmp = SNMPAgent("public");
 
+unsigned long ota_progress_millis = 0; //elegant OTA
 int snmp_ds_temp1 = 255;
 int snmp_ds_temp2 = 255;
 char* str_ds_temp1;
 char* str_ds_temp2;
-void setupSNMP();
+int ds_timer = 0;
+int var_ds_temp1;
+int var_ds_temp2;
+int freezerTemp; // секция термостатов
+int coolerTemp;
+int freezerTolerance = 3;//
+int coolerTolerance = 3;//вариатор температуры
+int freezerCooldown = 10;//кулдаун компрессора в минутах
+int coolerCooldown = 10;//кулдаун компрессора
+unsigned long uptime; //uptime в секундах
+int overloadcounter;//счётчик обнуления millis()
+
+void setupWebserver();
+void onOTAEnd(bool success);
+void onOTAStart();
+void onOTAProgress(size_t current, size_t final);
 void setupWiFi();
+void setupSNMP();
+float get_ds_temperature(DeviceAddress deviceAddress);
 
 void setup() {
-  sensors.begin(); //initialise DS18b20 sensors
   Serial.begin(9600);
+  sensors.begin(); //initialise DS18b20 sensors
   Serial.print("Sensors detected: ");
   Serial.print(sensors.getDeviceCount(), DEC);
   if (!sensors.getAddress(ds_term1, 0)) Serial.println("Unable to find address for Device 0");
@@ -40,35 +63,81 @@ void setup() {
   str_ds_temp2 = (char*)malloc(6);
   setupWiFi();
   setupSNMP();
+  setupWebserver();
+  sensors.requestTemperatures(); //initial request
+  delay(100);
+  var_ds_temp1 = get_ds_temperature(ds_term1);
+  var_ds_temp2 = get_ds_temperature(ds_term2);
+  delay(500);
 }
 
-float get_ds_temperature(DeviceAddress deviceAddress);
 
 void loop() {
-  // int t1 = analogRead(A0);
-  // int t2 = analogRead(A3);
-  // Serial.print("t1: ");Serial.print(t1);Serial.print(" | t2: ");Serial.print(t2);
-  // Serial.println();
-  //28E01D87005949E7
-  //2812CF870015278A
-  sensors.requestTemperatures();
-  Serial.println("DS18b20:");
-  Serial.print("t1: ");
-  Serial.print(get_ds_temperature(ds_term1));
-  Serial.print("| t2: ");
-  Serial.print(get_ds_temperature(ds_term2));
-  Serial.println();
   
-  snmp_ds_temp1=int(get_ds_temperature(ds_term1));
-  snmp_ds_temp2=int(get_ds_temperature(ds_term2));
+  if (ds_timer > 10) {
+    sensors.requestTemperatures();
+    var_ds_temp1 = get_ds_temperature(ds_term1);
+    var_ds_temp2 = get_ds_temperature(ds_term2);
+    Serial.print("t1: ");
+    Serial.print(var_ds_temp1);
+    Serial.print(" | t2: ");
+    Serial.print(var_ds_temp2);
+    Serial.println();
+    ds_timer = 0;  
+  }
+  
+  snmp_ds_temp1=int(var_ds_temp1);
+  snmp_ds_temp2=int(var_ds_temp2);
+  
   snmp.loop();
-  delay(100);
+  server.handleClient();
+  ElegantOTA.loop();
+  ds_timer++; // таймер для получения температуры, чтобы избежать delay(100) в лупе
+}
+
+void setupWebserver() {
+  server.on("/", []() {
+    server.send(200, "text/plain", "Hi! This is ElegantOTA Demo.");
+  });
+  ElegantOTA.begin(&server);    // Start ElegantOTA
+  // ElegantOTA callbacks
+  ElegantOTA.onStart(onOTAStart);
+  ElegantOTA.onProgress(onOTAProgress);
+  ElegantOTA.onEnd(onOTAEnd);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void onOTAEnd(bool success) {
+  // Log when OTA has finished
+  if (success) {
+    Serial.println("OTA update finished successfully!");
+  } else {
+    Serial.println("There was an error during OTA update!");
+  }
+  // <Add your own code here>
+}
+
+void onOTAStart() {
+  // Log when OTA has started
+  Serial.println("OTA update started!");
+  // <Add your own code here>
+}
+
+void onOTAProgress(size_t current, size_t final) {
+  // Log every 1 second
+  if (millis() - ota_progress_millis > 1000) {
+    ota_progress_millis = millis();
+    Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\n", current, final);
+  }
 }
 
 float get_ds_temperature(DeviceAddress deviceAddress) {
   float tempC = sensors.getTempC(deviceAddress);
   if (tempC == DEVICE_DISCONNECTED_C) {
-    return 255;
+    return -255;
+    delay(50);
   }
   else {return tempC;}
 }
